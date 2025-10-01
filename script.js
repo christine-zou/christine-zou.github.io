4a5bdaedbd723fc231da64ef98162f5aca661582
christine-zou <60272517+christine-zou@users.noreply.github.com>
2025-09-30 18:54:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,30 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function openTab(evt, tabId) {
+  let i, tabcontent, tablinks;
+
+  // Hide all tab contents
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabcontent[i].classList.remove("active");
+  }
+
+  // Remove "active" class from all tabs
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  // Show current tab
+  document.getElementById(tabId).style.display = "block";
+  document.getElementById(tabId).classList.add("active");
+  evt.currentTarget.classList.add("active");
+}
+
+// Show the first tab by default
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".tab").click();
+});
